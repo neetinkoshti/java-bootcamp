cf5f87a9331099cee6df64748d0fe844ae46d31a
neetinkoshti <neetin.koshti@gmail.com>
2024-12-29 01:37:04 +0530

    Remove Duplicate from a String

---
+++ ntn/com/RemoveDuplicateFromAString.java
@@ -0,0 +1,39 @@
+package ntn.com;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicateFromAString {
+
+    public static void main(String[] args) {
+
+        String originalString = "programmer";
+        removeDuplicateString(originalString);
+
+        String sentence = "this is nitin this is java developer";
+        removeDuplicateStringFromSentence(sentence);
+
+
+    }
+
+    private static void removeDuplicateString(String originalString){
+
+        Set<String> set = new HashSet<>();
+        String uniqueString = Arrays.stream(originalString.split(""))
+                .filter(a-> set.add(a))
+                .collect(Collectors.joining(""));
+        System.out.println(uniqueString);
+    }
+
+    private static void removeDuplicateStringFromSentence(String originalString){
+
+        Set<String> set = new HashSet<>();
+        String uniqueString = Arrays.stream(originalString.split(" "))
+                .filter(a-> set.add(a))
+                .collect(Collectors.joining(" "));
+        System.out.println(uniqueString);
+    }
+
+}
