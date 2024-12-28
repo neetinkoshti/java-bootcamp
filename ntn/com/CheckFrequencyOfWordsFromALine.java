cb1becad4837974050e80b963a7523b716741594
neetinkoshti <neetin.koshti@gmail.com>
2024-12-29 01:48:48 +0530

    check Frequency of words from a line

---
+++ ntn/com/CheckFrequencyOfWordsFromALine.java
@@ -0,0 +1,26 @@
+package ntn.com;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CheckFrequencyOfWordsFromALine {
+
+    public static void main(String[] args) {
+
+        String line = "this is nitin this is java developer";
+
+        checkFrequency(line);
+    }
+
+    private static void checkFrequency(String line){
+
+        Map<String,Long> map = Arrays.stream(line.split(" "))
+                .collect(Collectors.groupingBy(Function.identity() , LinkedHashMap::new , Collectors.counting()));
+
+        System.out.println(map);
+
+    }
+}
