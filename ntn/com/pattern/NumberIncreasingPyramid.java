67c685b4166086bb5d3cfd60eefc92a9b975addd
neetinkoshti <neetin.koshti@gmail.com>
2025-01-21 15:00:41 +0530

    Pattern Programming : NumberIncreasingPyramid

---
+++ ntn/com/pattern/NumberIncreasingPyramid.java
@@ -0,0 +1,28 @@
+package ntn.com.pattern;
+
+/**
+ *  1
+ *  1 2
+ *  1 2 3
+ *  1 2 3 4
+ *  1 2 3 4 5
+ *  1 2 3 4 5 6
+ */
+public class NumberIncreasingPyramid {
+
+    public static void main(String[] args) {
+
+        int n =6;
+        printNumberIncreasingPyramid(n) ;
+    }
+
+    private static void printNumberIncreasingPyramid(int n) {
+
+        for(int i=1;i<=n; i++){
+            for(int j=1 ; j<= i ;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
